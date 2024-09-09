cc4687c271dc43f709f5f636c8253feb80bf5183
Satheesh Kannan <satheeshkannan.ios@gmail.com>
2024-09-09 21:04:43 +0530

    feat: add spm support for customerio integration (#10)
    * chore: add package.swift file
    
    * chore: add package resolved file
    
    * chore: update README.md

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Rudder-CustomerIO",
+    platforms: [
+        .iOS("13.0")
+    ],
+    products: [
+        .library(
+            name: "Rudder-CustomerIO",
+            targets: ["Rudder-CustomerIO"]),
+    ],
+    dependencies: [
+        .package(name: "Customer.io", url: "https://github.com/customerio/customerio-ios", .exact("3.2.2")),
+        .package(name: "Rudder", url: "https://github.com/rudderlabs/rudder-sdk-ios", "1.29.0"..<"2.0.0")
+    ],
+    targets: [
+        .target(
+            name: "Rudder-CustomerIO",
+            dependencies: [
+                .product(name: "DataPipelines", package: "Customer.io"),
+                .product(name: "Rudder", package: "Rudder"),
+            ],
+            path: "Rudder-CustomerIO",
+            sources: ["Classes/"],
+            publicHeadersPath: "Classes/",
+            cSettings: [
+                .headerSearchPath("Classes/")
+            ]
+        )
+    ]
+)
